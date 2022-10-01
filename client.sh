37afc59212007f9e7c5c8ec55460b43255c0cd65
Humberto <hldtux@gmail.com>
2022-10-01 13:21:55 +0200

    inline function

--- client.sh
+++ client.sh
@@ -1,4 +1,54 @@
 #/bin/bash
 
-source ./common.sh
+function download_and_run(){
+
+    TYPE="${1:-server}"
+    TAG="${2:-v0.1}"
+
+    tmp_dir=$(mktemp -d -t ci-XXXXXXXXXX)
+    cd $tmp_dir
+
+    case "$(uname -s)" in
+
+    Darwin)
+        echo 'Mac OS X'
+
+        curl -s -L --output - "https://github.com/humbertodias/unity-netcode-helloworld/releases/download/${TAG}/StandaloneOSX.zip" | bsdtar -xf-
+
+        cd ./StandaloneOSX.app/Contents/MacOS
+        chmod +x Unity-Netcode-Hello-World
+        ./Unity-Netcode-Hello-World -mlapi $TYPE
+        
+        ;;
+
+    Linux)
+        echo 'Linux'
+
+        curl -s -L "https://github.com/humbertodias/unity-netcode-helloworld/releases/download/${TAG}/StandaloneLinux64.zip" --output StandaloneLinux64.zip
+        unzip StandaloneLinux64.zip
+        rm StandaloneLinux64.zip
+        chmod +x StandaloneLinux64
+        ./StandaloneLinux64 -mlapi $TYPE
+
+        ;;
+
+    CYGWIN*|MINGW32*|MSYS*|MINGW*)
+        echo 'MS Windows'
+
+        curl -s -L -k "https://github.com/humbertodias/unity-netcode-helloworld/releases/download/${TAG}/StandaloneWindows.zip" --output StandaloneWindows.zip
+        unzip StandaloneWindows.zip
+        rm StandaloneWindows.zip
+        ./StandaloneWindows.exe -mlapi $TYPE
+
+        ;;
+
+    *)
+        echo 'Unsupported OS' 
+        ;;
+    esac
+
+    rm -rf $tmp_dir
+
+}
+
 download_and_run client v0.1
